ac464ff53c500ce627f9d2a7ba6b3a7d563f82ee
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-08 20:43:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,11 @@
 class Shoe
-  attr_accessor :color, :size, :material
-    attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-    def initialize(brand)
-      @brand = brand
-    end
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+
+  end

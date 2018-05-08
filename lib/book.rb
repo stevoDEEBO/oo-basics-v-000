f0dea9740e5a5474218cfbb68f267ecc42847442
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-08 20:36:43 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,33 +1,33 @@
 class Book
- 
+
   def initialize(title)
     @title = title
   end
- 
+
   def title
     @title
   end
- 
+
   def author=(author)
     @author = author
   end
- 
+
   def author
     @author
   end
- 
+
   def page_count=(num)
     @page_count = num
   end
- 
+
   def page_count
     @page_count
   end
- 
+
   def genre=(genre)
     @genre = genre
   end
- 
+
   def genre
     @genre
   end
@@ -36,5 +36,5 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
- 
+
 end
